f79b685aea7616680d3a299419ec90e57fe06afc
Ryan Wesslen <ryan@explosion.ai>
2023-06-27 18:30:39 -0400

    update prodigy.sh

--- template/prodigy.sh
+++ template/prodigy.sh
@@ -1,1 +1,8 @@
-python -m prodigy rlhf.respond rlhf_data data/dataset.jsonl -F recipe.py
+# Activate venv
+source /opt/venv/bin/activate
+
+# Make sure prodigy.json has appropriate environment variables
+python scripts/mkconfig.py
+
+# Run command
+python -m prodigy ner.manual news_ner blank:en data/dataset.jsonl --label ORG,PERSON,GPE 
