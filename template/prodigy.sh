4a71c849e2e9031776fdd667016152f802dad598
Ryan Wesslen <ryan@explosion.ai>
2023-06-27 21:33:12 -0400

    add spancat project to sh

--- template/prodigy.sh
+++ template/prodigy.sh
@@ -1,8 +1,14 @@
 # Activate venv
 source /opt/venv/bin/activate
 
+# clone repo
+spacy project clone tutorials/spancat_food_ingredients
+
+# cd to folder
+cd spancat_food_ingredients
+
 # Make sure prodigy.json has appropriate environment variables
-python scripts/mkconfig.py
+python ../scripts/mkconfig.py
 
 # Run command
-python -m prodigy ner.manual news_ner blank:en data/dataset.jsonl --label ORG,PERSON,GPE 
+python -m spacy project run span_manual
